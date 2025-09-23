04b7ab157a88b4a8f7920bbd79a2704511e51dc2
Gabriel-Dev2024 <gabrielprudencio2008@gmail.com>
2025-09-23 19:13:23 -0300

    hotfix(api) err debug message

--- backend/src/index.js
+++ backend/src/index.js
@@ -80,7 +80,7 @@ if (process.env.NODE_ENV === "production") {
 
 // error handler
 app.use((err, req, res, next) => {
-	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : console.log(err) });
+	res.status(500).json({ message: process.env.NODE_ENV === "production" ? console.log(err) & "Internal server error" : err.message });
 });
 
 httpServer.listen(PORT, () => {
