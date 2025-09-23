63bd260049859e3dcac7c750b6d47032f624724b
Gabriel-Dev2024 <gabrielprudencio2008@gmail.com>
2025-09-23 19:11:23 -0300

    refactore(api) error message fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -80,7 +80,7 @@ if (process.env.NODE_ENV === "production") {
 
 // error handler
 app.use((err, req, res, next) => {
-	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : console.log(err.message) });
+	res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : console.log(err) });
 });
 
 httpServer.listen(PORT, () => {
